3c6f4b1aa23140fc73ccdbe60adf5272ba5c15ef
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-03-25 20:27:21 +0300

    feat: Реструктуризация приложения

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,19 @@
-import express from 'express'; // Импортируем фреймворк для работы с API
-import cors from 'cors'; // Импортируем CORS для разрешения запросов с других доменов
-import dotenv from 'dotenv'; // Импортируем dotenv для загрузки переменных окружения из файла .env
-import pino from 'pino'; // Импортируем логгер
+import dotenv from 'dotenv';
+import express from 'express';
+import { logRoutes } from './bootstrap/log-routes';
+import logger from './logger/pino.logger';
+import satelliteController from './modules/satellite/satellite.controller';
 
-import satelliteRoutes from './routes/satelliteRoutes';
+dotenv.config();
+const app = express();
 
-dotenv.config(); // Загружаем переменные окружения из .env
-const app = express(); // Создаем экземпляр Express-приложения
-const logger = pino(); // Создаем экземпляр логгера
+app.use(express.json());
 
-app.use(cors()); // Разрешаем запросы с других доменов (по умолчанию разрешает все)
-app.use(express.json()); // Позволяет Express работать с JSON-данными в запросах
-app.use('/api/satellites', satelliteRoutes);
+app.use('satellite', satelliteController);
 
-// Основной маршрут (проверка работоспособности API)
-app.get('/', (req, res) => {
-  logger.info('GET /'); // Логируем запрос в консоли
-  res.json({ message: 'Satellite API is running' }); // Отправляем JSON-ответ клиенту
-});
+logRoutes(app);
 
-// Определяем порт для сервера
-const PORT = process.env.PORT || 3000; // Берем порт из .env, если нет — используем 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  logger.info(`Server running on http://localhost:${PORT}`); // Логируем запуск сервера
+  logger.info(`Server running on http://localhost:${PORT}`);
 });
