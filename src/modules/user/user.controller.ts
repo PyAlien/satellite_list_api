16eb5c352c9d0254b404ba82a3109307f04249d7
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-04-09 11:44:51 +0300

    feat: Добавлена работа с DTO на примере регистрации

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,20 @@
 import { Request, Response, Router } from 'express';
+import { validate } from '../../validate';
+import { UserRegisterDto } from './dto';
 import { userService } from './user.service';
 
 export const userController = Router();
 
 userController.post('/register', (req: Request, res: Response) => {
-  const user = userService.register(req.body);
+  const instance = validate(UserRegisterDto, req.body);
+
+  const user = userService.register(instance);
   res.json(user);
 });
 
 userController.post('/login', (req: Request, res: Response) => {
   const { username, password } = req.body;
+
   const user = userService.login(username, password);
   res.json(user);
 });
