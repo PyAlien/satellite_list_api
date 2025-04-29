56a6d2373aecdd27c51898c30308bfd08989f136
PyAlien <codeproger@yandex.ru>
2025-04-29 20:04:48 +0300

    feat: валидация при создании и регистрации пользователя

---
+++ src/modules/user/dto/user-reg.dto.ts
@@ -0,0 +1,11 @@
+﻿import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class UserRegDto {
+  @IsString()
+  name: string;
+  @IsEmail()
+  email: string;
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
