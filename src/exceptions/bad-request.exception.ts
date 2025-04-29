0c6360a308c7bc704147ac9c90ec9ec3caa17d68
PyAlien <codeproger@yandex.ru>
2025-04-29 20:02:33 +0300

    feat: добавил кастомные классы ошибок

---
+++ src/exceptions/bad-request.exception.ts
@@ -0,0 +1,7 @@
+﻿export class BadRequestException extends Error {
+  public readonly status = 400;
+
+  constructor(message: string = `Неправильно выполнен запрос`) {
+    super(message);
+  }
+}
---
+++ src/exceptions/index.ts
@@ -0,0 +1,4 @@
+﻿export { ConflictException } from './conflict.exception';
+export { NotFoundException } from './not-found.exception';
+export { BadRequestException } from './bad-request.exception';
+export { UnauthorisedException } from './unauthorised.exception';
